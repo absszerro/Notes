d3d49a65f141da242c7b14dc4c53cfc10da210f6
absszerro <cryinghostt.56@gmail.com>
2022-12-15 17:17:59 +0300

    first

---
+++ prog/MainWindow.py
@@ -0,0 +1,177 @@
+import tkinter as tk
+from tkinter import *
+import customtkinter
+import customtkinter as ctk
+from PIL import Image, ImageTk
+
+HEIGHT = 580
+WIDTH = 1100
+
+ctk.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
+ctk.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
+
+
+class MainWindow(ctk.CTk):
+    def __init__(self, ):
+        super().__init__()
+
+        # self.check = [False, False, False, False, ]
+        # self.check_1 = False
+        # self.check_2 = False
+        # self.check_3 = False
+        # self.check_4 = False
+        # self.check_5 = False
+
+        icon_home = ctk.CTkImage(Image.open('icons/home.png'), size=(20, 20))
+        icon_archive = ctk.CTkImage(Image.open('icons/archive.png'), size=(20, 20))
+        icon_statistics = ctk.CTkImage(Image.open('icons/statistics.png'), size=(20, 20))
+        icon_settings = ctk.CTkImage(Image.open('icons/settings.png'), size=(20, 20))
+
+        # create a window
+        self.title('Planner')
+        self.geometry(f"{WIDTH}x{HEIGHT}")
+        # self.home_frame = None
+
+        # left panel frame
+        self.left_panel_frame = ctk.CTkFrame(self, width=WIDTH // 100 * 5, height=HEIGHT)
+        self.left_panel_frame.pack(fill=tk.Y, side=tk.LEFT)
+        self.home_button = ctk.CTkButton(self.left_panel_frame,
+                                         text='Home',
+                                         image=icon_home,
+                                         fg_color='transparent',
+                                         corner_radius=0,
+                                         hover_color=("gray70", "gray30"),
+                                         anchor="w",
+                                         border_spacing=10,
+                                         # compound='LEFT',
+                                         command=self.clicked_home_button)
+        self.home_button.pack(fill=tk.X, side=tk.TOP)
+        self.archive_button = ctk.CTkButton(self.left_panel_frame,
+                                            text='Archive',
+                                            image=icon_archive,
+                                            fg_color='transparent',
+                                            corner_radius=0,
+                                            anchor="w",
+                                            border_spacing=10,
+                                            hover_color=("gray70", "gray30"),
+                                            command=self.clicked_archive_button)
+        self.archive_button.pack(fill=tk.X, side=tk.TOP)
+        self.statistics_button = ctk.CTkButton(self.left_panel_frame,
+                                               text='Statistics',
+                                               image=icon_statistics,
+                                               fg_color='transparent',
+                                               corner_radius=0,
+                                               anchor="w",
+                                               border_spacing=10,
+                                               hover_color=("gray70", "gray30"),
+                                               command=self.clicked_statistics_button)
+        self.statistics_button.pack(fill=tk.X, side=tk.TOP)
+        # self.events_button = ctk.CTkButton(self.left_panel_frame, text='events')
+        # self.events_button.pack(fill=tk.X, side=tk.TOP)
+        self.settings_button = ctk.CTkButton(self.left_panel_frame,
+                                             text='Settings',
+                                             image=icon_settings,
+                                             fg_color='transparent',
+                                             corner_radius=0,
+                                             anchor="w",
+                                             border_spacing=10,
+                                             hover_color=("gray70", "gray30"),
+                                             command=self.clicked_settings_button)
+        self.settings_button.pack(fill=tk.X, side=tk.BOTTOM)
+
+        # today frame
+        self.home_frame = self.create_home_frame()
+
+        # archive frame
+        self.archive_frame = self.create_archive_frame()
+
+        # statistics frame
+        self.statistics_frame = self.create_statistics_frame()
+
+        # settings frame
+        self.settings_frame = self.create_settings_frame()
+
+    def create_home_frame(self, ):
+        return ctk.CTkFrame(self, width=WIDTH, height=HEIGHT, corner_radius=0, fg_color='red', )
+
+    def create_archive_frame(self, ):
+        frame = ctk.CTkFrame(self, width=WIDTH, height=HEIGHT, corner_radius=0, )
+        return frame
+
+    def create_statistics_frame(self):
+        return ctk.CTkFrame(self, width=WIDTH, height=HEIGHT, corner_radius=0, fg_color='green')
+
+    def create_settings_frame(self):
+        return ctk.CTkFrame(self, width=WIDTH, height=HEIGHT, corner_radius=0, fg_color='blue')
+
+    def select_frame_by_name(self, name):
+        self.home_button.configure(fg_color=("gray75", "gray25") if name == "home" else "transparent")
+        self.archive_button.configure(fg_color=("gray75", "gray25") if name == "frame_2" else "transparent")
+        self.statistics_button.configure(fg_color=("gray75", "gray25") if name == "frame_3" else "transparent")
+        self.settings_button.configure(fg_color=("gray75", "gray25") if name == "frame_3" else "transparent")
+
+        # show selected frame
+        if name == "home":
+            self.home_frame.pack(fill=tk.X, side=tk.RIGHT)
+        else:
+            self.home_frame.pack_forget()
+        if name == "archive":
+            self.archive_frame.pack(fill=tk.X, side=tk.RIGHT)
+        else:
+            self.archive_frame.pack_forget()
+        if name == "statistics":
+            self.statistics_frame.pack(fill=tk.X, side=tk.RIGHT)
+        else:
+            self.statistics_frame.pack_forget()
+        if name == "settings":
+            self.settings_frame.pack(fill=tk.X, side=tk.RIGHT)
+        else:
+            self.settings_frame.pack_forget()
+
+    # def close_all_frame(self, ):
+    #     self.home_frame.pack_forget()
+    #     self.archive_frame.pack_forget()
+    #     self.statistics_frame.pack_forget()
+    #     self.settings_frame.pack_forget()
+    #     for i in range(0, len(self.check)):
+    #         self.check[i] = False
+
+    def clicked_home_button(self):
+        # if not self.check[0]:
+        #     self.close_all_frame()
+        #     self.home_frame.pack(fill=tk.X, side=tk.RIGHT)
+        #     self.check[0] = True
+        # else:
+        #     self.home_frame.pack_forget()
+        #     self.check[0] = False
+        self.select_frame_by_name('home')
+
+    def clicked_archive_button(self):
+        # if not self.check[1]:
+        #     self.close_all_frame()
+        #     self.archive_frame.pack(fill=tk.X, side=tk.RIGHT)
+        #     self.check[1] = True
+        # else:
+        #     self.archive_frame.pack_forget()
+        #     self.check[1] = False
+        self.select_frame_by_name('archive')
+
+    def clicked_statistics_button(self):
+        # if not self.check[2]:
+        #     self.close_all_frame()
+        #     self.statistics_frame.pack(fill=tk.X, side=tk.RIGHT)
+        #     self.check[2] = True
+        # else:
+        #     self.statistics_frame.pack_forget()
+        #     self.check[2] = False
+        self.select_frame_by_name('statistics')
+
+    def clicked_settings_button(self):
+        # if not self.check[3]:
+        #     self.close_all_frame()
+        #     self.settings_frame.pack(fill=tk.X, side=tk.RIGHT)
+        #     self.check[3] = True
+        # else:
+        #     self.settings_frame.pack_forget()
+        #     self.check[3] = False
+        self.select_frame_by_name('settings')
