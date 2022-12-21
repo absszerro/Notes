066bf53cf849d3e062c773bcaa41fe60ff04785d
absszerro <cryinghostt.56@gmail.com>
2022-12-21 11:58:56 +0300

    add Params

---
+++ prog/Params.py
@@ -0,0 +1,10 @@
+HEIGHT = 700
+WIDTH = 1200
+
+COLORS = [('#ED8C8A', '#FF9494'),
+          ('#EDE0A4', '#FFF2B0'),
+          ('#DA95EB', '#ECA1FF'),
+          ('#7CEBA1', '#87FFA9'),
+          ('#8988EB', '#9A94FF')]
+
+DARK_TEXT_COLOR = '#272138'
