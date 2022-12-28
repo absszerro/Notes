23ea410ef66dd8891b8af4712cf8d48e87c560dc
ErmilovArtem <116365455+ErmilovArtem@users.noreply.github.com>
2022-12-28 09:54:04 +0300

    End Of all

--- prog/List_of_NuT.py
+++ prog/List_of_NuT.py
@@ -1,10 +1,11 @@
 import pickle
 from datetime import datetime
 import matplotlib.pyplot as plt
-from NuT import NuT
+from Notes.prog.NuT import NuT
+
 
 class List_of_NuT:
-    def __init__(self, date=datetime.now().strftime("%d.%m.%Y"), list_of_nuts = []):
+    def __init__(self, date=datetime.now().strftime("%d.%m.%Y"), list_of_nuts=[]):
         self.list_of_tags = set()
         if date != datetime.now().strftime("%d.%m.%Y"):
             try:
@@ -26,6 +27,7 @@ class List_of_NuT:
         nt_fo_type = NuT()
         for elem in list_of_nuts:
             if type(elem) != type(nt_fo_type):
+                print(type(elem), type(nt_fo_type))
                 list_of_nuts.remove(elem)
                 print("ONE OF VALUE NOT IS NUT! THS VAL WAS DEL!")
         self.list_of_nuts = list_of_nuts
@@ -33,28 +35,44 @@ class List_of_NuT:
         if self.list_of_nuts != []:
             self.tags_appender(self.list_of_tags, self.list_of_nuts)
 
+        self.all_checked, self.true_checked, self.false_checked = self.stats_give()
+
+    def stats_give(self):
+        counter, tr, fl = 0, 0, 0
+        for eleme in self.list_of_nuts:
+            if eleme.type in [2, 3]:
+                counter += 1
+                if not eleme.check:
+                    tr += 1
+                else:
+                    fl += 1
+        return (counter, tr, fl)
+
     def tags_appender(self, list_of_tags, list_of_nuts):
         for elem in list_of_nuts:
             for tag in elem.tags:
                 list_of_tags.add(tag)
         sorted(list_of_tags)
 
-    def NuTs_appender(self, message="", type=0, check=False, datetime=datetime.now().strftime("%H:%M"), NuTs = None):
+    def NuTs_appender(self, message="", type=0, check=False, datetime=datetime.now().strftime("%H:%M"), NuTs=None):
         if NuTs == None:
             self.list_of_nuts.append(NuT(message, type, check, datetime))
         else:
             self.list_of_nuts.append(NuTs)
         self.tags_appender(self.list_of_tags, self.list_of_nuts)
 
-    def give_prevue(self, n = 40, flag_of_date = False):
-        message = ""
-        for elem in self.list_of_nuts:
-            if len(message) < n:
-                message += elem.message + "\n"
+    def give_prevue(self, n=40, flag_of_date=False, give_text=True):
+            message = ""
+            for elem in self.list_of_nuts:
+                if len(message) < n:
+                    message += elem.message + "\n"
+                else:
+                    break
+            message = message[:n]
+            if give_text:
+                if flag_of_date:
+                    return message + "\t" * 5 + "|" + self.date + "|" + "\n"
+                else:
+                    return message
             else:
-                break
-        message = message[:n]
-        if flag_of_date:
-            return message + "\t" * 5 + "|" + self.date + "|" + "\n"
-        else:
-            return message
+                return [self.date, str(self.true_checked) + "/" + str(self.all_checked), message]
---
+++ prog/mainBack.py
@@ -0,0 +1,58 @@
+from Notes.prog.NuT import NuT
+from Notes.prog.List_of_NuT import List_of_NuT
+from Notes.prog.All_Nut import All_Nut
+
+N = NuT("#start with     #Som#e Text and #tags", type=2, check=True)
+B = NuT("Yea", type=2, check=False)
+C = NuT("j#wow this work", type=3, check=(True, False, True))
+D = NuT("For Test")
+Na = NuT("#start with     #Som#e Text and #tags", type=2, check=False)
+Nb = NuT("#start with     #Som#e Text and #tags", type=2, check=False)
+Nc = NuT("#start with     #Som#e Text and #tags", type=2, check=True)
+Nd = NuT("#start with     #Som#e Text and #tags", type=2, check=True)
+Ne = NuT("#start with     #Som#e Text and #tags, #and", type=3, check=(False, False, False))
+print(N.tags)
+print(N.give_with_date_and_sl_n())
+
+Ln = List_of_NuT("01.02.2002", [N, B])
+Lb = List_of_NuT("01.02.2025", [C, D])
+La = List_of_NuT("01.02.2002", [Na, Nb, Nc, Nd, Ne])
+
+print(Ln.list_of_tags)
+print(Lb.list_of_tags)
+print(Lb.list_of_nuts)
+print(Lb.date)
+print(Lb.give_prevue(n=400))
+
+Al = All_Nut([Ln, La])
+
+print(Al.list_of_nuts, 111)
+print(Al.all_nuts_tags, 111)
+
+print(Lb.list_of_tags, 111)
+Al.all_nut_appender(list_of_nuts=Lb)
+
+print(Al.list_of_nuts, 111)
+
+print("\n")
+print(Al.all_nuts_tags)
+print(Al.sorted_by_date())
+print(Al.searcher("#Futures_plans"), "what")
+
+print(La.give_prevue(n=100, give_text=False))
+
+List_of_Nu = List_of_NuT()
+List_of_Nu.NuTs_appender(message="lol", type=2)
+List_of_Nu.NuTs_appender(message="e #lole", type=3)
+
+print(List_of_Nu.list_of_nuts[0].message)
+
+
+
+# Al.save_file_by_pikle(Al)
+
+# Bl = Al.load_file_by_pikle()
+
+# print(Bl.searcher("#wow"), "wtf")
+
+# print(Al.give_stats())
